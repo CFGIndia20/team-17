e4f9d403707008d404807a3193bf22f65f52be79
DeepiakPomendkar <deepika@11054>
2020-07-25 22:45:21 +0530

    Backend connected

--- apis/complaint/views.py
+++ apis/complaint/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from firebase import getReferences
 # Create your views here.
+import json
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.http import HttpResponse
-auth, database = getReferences()
+auth, database,bot = getReferences()
+from . import ml
+
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+cred = credentials.Certificate("./fire1.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+
+
 
 
 @api_view(['GET', 'POST'])
@@ -16,49 +29,106 @@ def postComplaint(request):
     if request.method == 'GET':
         # snippets = Snippet.objects.all()
         # # serializer = SnippetSerializer(snippets, many=True)
-        # if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        #     if not request.args.get("hub.verify_token") == "hello":
-        #         return "Verification token mismatch", 403
-        #     return request.args["hub.challenge"], 200
-        return "Hello world", 200
-        # return Response("Hello world", 200)
+        if request.query_params.get("hub.mode") == "subscribe" and request.query_params.get("hub.challenge"):
+            if not request.query_params.get("hub.verify_token") == "hello":
+                return HttpResponse("Verification token mismatch", 403)
+            print("***********************",request.query_params["hub.challenge"])
+            return HttpResponse(request.query_params["hub.challenge"], 200)
+        return HttpResponse("Hello world", 200)
+        # print(request.data,request.query_params)
+        # return Response("Hello", 200)
 
     elif request.method == 'POST':
+        received_json_data = json.loads(request.body.decode("utf-8"))
+        print(received_json_data)
+        print("***********************")
 
-        value={
-        "text":"There is a lot of garbage in my area",
-        "lat":78.92,
-        "long":93.76,
-        "category":"Garbage Collection",
-        "user_identity":"1"
-        }
+        # data = request.get_json()
+        #
+        # print(data)
 
-        database.child('user').child("complaints").push(value)
 
-        if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-            if not request.args.get("hub.verify_token") == "hello":
-                return "Verification token mismatch", 403
-            return request.args["hub.challenge"], 200
-        return "Hello world", 200
-        # return HttpResponse(request.data)
-        # serializer = SnippetSerializer(data=request.data)
-        # if serializer.is_valid():
-        #     serializer.save()
-            # return Response(serializer.data, status=status.HTTP_201_CREATED)
-        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-# def postComplaint(request):
 
-#     value={
-#         "text":"There is a lot of garbage in my area",
-#         "lat":78.92,
-#         "long":93.76,
-#         "category":"Garbage Collection",
-#         "user_identity":"1"
-#     }
+        print(request.data["object"])
 
-#     database.child('user').child("complaints").push(value)
 
-#     return HttpResponse('Inserted')
+        data=request.data
 
+        if data['object'] == 'page':
+            for entry in data['entry']:
+                for messaging_event in entry['messaging']:
+
+                    # IDs
+                    sender_id = messaging_event['sender']['id']
+                    recipient_id = messaging_event['recipient']['id']
+
+                    if messaging_event.get('message'):
+                        # Extracting text message
+                        if 'text' in messaging_event['message']:
+                            messaging_text = messaging_event['message']['text']
+                            listOfCategory = ml.predict([messaging_text])
+
+                            ##### add lat long
+                            value = {
+                                "text": messaging_event['message']['text'],
+                                "lat": 78.92,
+                                "long": 93.76,
+                                "category": "Garbage Collection",
+                                "user_identity": sender_id
+                            }
+
+                            database.child('user').child("complaints").push(value)
+                        else:
+                            messaging_text = 'no text'
+
+                        # Echo
+                        response = messaging_text
+                        bot.send_text_message(sender_id, listOfCategory[0])
+        print(request.data)
+        print("^^^^^^^^^^^^^^^^^^")
+        return HttpResponse("hi", 200)
+        # return HttpResponse(request.data, 200)
+
+
+def firestore1(request):
+
+    userId = "123456"
+
+
+
+    users = database.child("user").get()
+
+    for user in users.val():
+
+        if user['id']==userId:
+            long = 10.67
+            value = db.collection('user').document(user['id']).collection("complaints").get().to_dict()
+
+            print("%%%%%%%%%%%%%%5")
+            value["long"]=long
+            db.collection('user').document(user['id']).collection("complaints").set(value)
+
+        else:
+
+            doc_ref = db.collection('user').document(userId).collection("complaints")
+
+            val = doc_ref.add({
+                'text': 'This is bad area',
+                'lat': 20.78,
+                'long': 40.67,
+                'category': "Garbage",
+                "status": 0
+
+            })
+
+            ###addkey
+            key = val[1].id
+            print("^^^^^^^^^^^^^^^^^^^^^^6", val[1].id)
+
+            database.child("user").child(userId).set({'complaint_id': key})
+
+
+
+    return HttpResponse("done")
 
 
--- apis/firebase.py
+++ apis/firebase.py
@@ -1,19 +1,26 @@
 import pyrebase
-
+from pymessenger import Bot
 firebaseconfig = {
-    "apiKey": "AIzaSyDgFX_9PtztpFf-d71uEcBr43SWth-EVcs",
-    "authDomain": "janagraha-cce3a.firebaseapp.com",
-    "databaseURL": "https://janagraha-cce3a.firebaseio.com",
-    "projectId": "janagraha-cce3a",
-    "storageBucket": "janagraha-cce3a.appspot.com",
-    "messagingSenderId": "27343948050",
-    "appId": "1:27343948050:web:563df56c0c4a90b2558bcb",
-    "measurementId": "G-J7RS6NV8G2"
+    "apiKey": "AIzaSyAH1hNhcetH_YvEalGRRjCIJPQXRNeUg_o",
+    "authDomain": "labcoordinator-b2bb3.firebaseapp.com",
+    "databaseURL": "https://labcoordinator-b2bb3.firebaseio.com",
+    "projectId": "labcoordinator-b2bb3",
+    "storageBucket": "labcoordinator-b2bb3.appspot.com",
+    "messagingSenderId": "602115928200",
+    "appId": "1:602115928200:web:2d6a59967fdd781e626f3d",
+    "measurementId": "G-3BK35C41MG"
+
 }
+
+PAGE_ACCESS_TOKEN = "EAA09YRWYyQUBAMrjyLJBDcqygPbaDHgvPNYVQtLsZC2rsXONec2j4Iqu9b4WKaXBJ6nW9THC4Qs7DN4ZCxFb0zLljpZBTa6bIk28CXXJnMUgqRxcxglskfuVWCjb9jP2KZBI8FBrVUk1xOqn2vFzKEqwZBr4Cqk2vapcJSyPuacpSEpjJQtLj"
+
+bot = Bot(PAGE_ACCESS_TOKEN)
 
 firebase = pyrebase.initialize_app(firebaseconfig)
 auth = firebase.auth()
 database = firebase.database()
 
 def getReferences():
-    return auth, database
+
+
+    return auth, database,bot
