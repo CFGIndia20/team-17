6996262c6248c5f0952b41f319bd4c02cee8e607
Aditya Srivastava <adityasrivastava301199@gmail.com>
2020-07-26 04:26:13 +0530

    Add video url for testing

--- apis/complaint/utils.py
+++ apis/complaint/utils.py
@@ -34,4 +34,8 @@ def convert_to_text(file_path):
     return text
 
 if __name__ == "__main__":
+    # audio
     convert_to_text("https://cdn.fbsbx.com/v/t59.3654-21/116270940_737214636820117_5925911564562160881_n.mp4/audioclip-1595706435000-2972.mp4?_nc_cat=102&_nc_sid=7272a8&_nc_ohc=K4r8hifvq9YAX8hmohz&_nc_ht=cdn.fbsbx.com&oh=946bc42d9532e101db566a3d899d3b90&oe=5F1E7001")
+
+    # video
+    convert_to_text("https://video.xx.fbcdn.net/v/t42.3356-2/116141357_4226319140743373_3554114261183604491_n.mp4/video-1595706505.mp4?_nc_cat=109&_nc_sid=060d78&_nc_ohc=ooMfoBxx__AAX9A_-G-&vabr=283412&_nc_ht=video.xx&oh=af594c827e2a1961274ad5b15ef7224c&oe=5F1DD9BC")
