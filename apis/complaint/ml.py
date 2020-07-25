695ce9fb2670c63e1107b787efa413ecaba2c922
Aditya Srivastava <adityasrivastava301199@gmail.com>
2020-07-25 18:28:06 +0530

    Add api for ml algo

---
+++ apis/complaint/ml.py
@@ -0,0 +1,26 @@
+import pickle
+from sklearn.linear_model import SGDClassifier
+
+with open('../../saved_models/sdg.pkl', 'rb') as fid:
+    sgd = pickle.load(fid)
+
+def predict(x_test):
+    """
+    Array of string : Description of the location / problem
+    returns the list of predicted categories
+    """
+    global sgd
+    return sgd.predict(x_test)
+
+### Just for testing ####
+# TODO: Delete before production
+if __name__ == "__main__":
+    print(
+        predict(
+            [
+                "Jogeshwari east subhash road ,\
+                there is no road as we can see only potholes\
+                every where on subhash road"
+            ]
+        )
+    )
